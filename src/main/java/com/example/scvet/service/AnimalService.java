0a528493d34dd6d6a3277e12e8c6ecee0f71b89b
adailton-junior <78552842+adailton-junior@users.noreply.github.com>
2021-06-04 18:21:12 -0300

    Update AnimalService.java

--- src/main/java/com/example/scvet/service/AnimalService.java
+++ src/main/java/com/example/scvet/service/AnimalService.java
@@ -1,4 +1,29 @@
 package com.example.scvet.service;
 
+import com.example.scvet.api.dto.AnimalDTO;
+import com.example.scvet.model.entity.Animal;
+import com.example.scvet.model.repository.AnimalRepository;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AnimalService {
+
+    private AnimalRepository repository;
+
+    public AnimalService(AnimalRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Animal> getAnimais(){
+        return repository.findAll();
+    }
+
+    public Optional<Animal> getAnimalById(Long id){
+        return repository.findById(id);
+    }
+
 }
