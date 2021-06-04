df3e3b396555c4a46edf0ab871ed8a8c914ab361
adailton-junior <78552842+adailton-junior@users.noreply.github.com>
2021-06-04 18:21:00 -0300

    Update Animal.java

--- src/main/java/com/example/scvet/model/entity/Animal.java
+++ src/main/java/com/example/scvet/model/entity/Animal.java
@@ -1,4 +1,27 @@
 package com.example.scvet.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
 public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAnimal;
+    private String nome;
+    private double peso;
+    private String sexo;
+
 }
