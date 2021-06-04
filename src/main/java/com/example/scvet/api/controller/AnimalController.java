bc93a82c9993693ad558145982da2841e12841cd
adailton-junior <78552842+adailton-junior@users.noreply.github.com>
2021-06-04 18:20:34 -0300

    Update AnimalController.java

--- src/main/java/com/example/scvet/api/controller/AnimalController.java
+++ src/main/java/com/example/scvet/api/controller/AnimalController.java
@@ -1,4 +1,34 @@
 package com.example.scvet.api.controller;
 
+
+import com.example.scvet.model.entity.Animal;
+import com.example.scvet.service.AnimalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/animais")
+@RequiredArgsConstructor
+
 public class AnimalController {
+    private final AnimalService service;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        List<Animal> animais = service.getAnimais();
+        return ResponseEntity.ok(animais);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id){
+        Optional<Animal> animal = service.getAnimalById(id);
+        return ResponseEntity.ok(animal);
+    }
 }
