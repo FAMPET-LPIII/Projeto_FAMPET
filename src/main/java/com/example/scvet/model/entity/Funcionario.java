152a97a491afffbf9527c70341f6c7bd40206af3
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-06-14 23:17:10 -0300

    Falci_Cliente_Funcionario

--- src/main/java/com/example/scvet/model/entity/Funcionario.java
+++ src/main/java/com/example/scvet/model/entity/Funcionario.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -29,7 +26,8 @@ public class Funcionario{
     private String uf;
     private String cep;
     private String telefone;
-
-
-
+    @ManyToOne
+    private Funcao funcao;
+    @ManyToOne
+    private Especialidade especialidade;
 }
