59588e685ab736d5ad98bdd22b15e8e7be48014f
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-06-15 20:36:31 -0300

    Revert "Revert "FalciUpdate""
    This reverts commit 96ec71a3cc5277fe73257e85ffd5cba8713e9e37.

--- src/main/java/com/example/scvet/api/dto/ClienteDTO.java
+++ src/main/java/com/example/scvet/api/dto/ClienteDTO.java
@@ -1,6 +1,7 @@
 package com.example.scvet.api.dto;
 
 import com.example.scvet.model.entity.Cliente;
+import com.example.scvet.model.entity.Pessoa;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,26 +14,12 @@ import org.modelmapper.ModelMapper;
 public class ClienteDTO {
 
     private Long idCliente;
-    private String nome;
-    private String email;
-    private String logradouro;
-    private int numero;
-    private String complemento;
-    private String bairro;
-    private String cidade;
-    private String uf;
-    private String cep;
-    private String telefone;
-    private Long idAnimal;
-    private String nomeAnimal;
-    //Ver necessidade de lista
+    private Pessoa pessoa;
 
     public static ClienteDTO create(Cliente cliente){
 
         ModelMapper modelMapper = new ModelMapper();
         ClienteDTO dto = modelMapper.map(cliente, ClienteDTO.class);
-//        assert dto.getIdAnimal().equals(cliente.getAnimal().getIdAnimal());
-//        assert dto.getNomeAnimal().equals(cliente.getAnimal().getNome());
         return dto;
     }
 
--- src/main/java/com/example/scvet/api/dto/PessoaDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.scvet.api.dto;
-
-public class PessoaDTO {
-}
