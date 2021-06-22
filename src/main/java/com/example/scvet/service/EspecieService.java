21ffa214e1512ec1415435cef672cf1fe3d7eaee
adailton-junior <78552842+adailton-junior@users.noreply.github.com>
2021-06-22 11:10:29 -0300

    Up_Controller _Service Adailton
    atualizando controller e Service

--- src/main/java/com/example/scvet/service/EspecieService.java
+++ src/main/java/com/example/scvet/service/EspecieService.java
@@ -1,14 +1,10 @@
 package com.example.scvet.service;
 
-import com.example.scvet.api.dto.EspecieDTO;
 import com.example.scvet.model.entity.Especie;
 import com.example.scvet.model.repository.EspecieRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-
 @Service
 public class EspecieService {
 
@@ -18,14 +14,12 @@ public class EspecieService {
         this.repository = repository;
     }
 
-    public List<EspecieDTO> getEspecies(){
-        List<EspecieDTO> list = repository.findAll().stream().map(EspecieDTO::create).collect(Collectors.toList());
-        return list;
+    public List<Especie> getEspecies(){
+        return repository.findAll();
     }
 
-    public EspecieDTO getEspecieById(Long id){
-        Optional<Especie> especie = repository.findById(id);
-        return especie.map(EspecieDTO::create).orElseThrow(() -> new RuntimeException("Especie não encontrada!"));
+    public Optional<Especie> getEspecieById(Long id){
+        return  repository.findById(id);
     }
 
 
